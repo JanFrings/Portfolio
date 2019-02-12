4eea6fce48356457049a33b9b8dc7746e0fea78e
Jan <Jan-Frings@gmx.de>
2019-02-12 11:06:58 +0100

    add social media icons

--- Portfolio/blog/urls.py
+++ Portfolio/blog/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
+    path('', views.PostIndexView.as_view(), name='post_index'),
     path('post/list/', views.PostListView.as_view(), name='post_list'),
     path('post/<int:pk>/detail/', views.PostDetailView.as_view(), name='post_detail'),
     path('post/create/', views.PostCreateView.as_view(), name='post_create'),
@@ -14,7 +15,6 @@ urlpatterns = [
     path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
     path('post/<int:pk>/comment/', views.add_comment_to_post, name='post_comment'),
     # path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
-    path('post/index', views.PostIndexView.as_view(), name='post_index'),
 
     path('login/', auth_view.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_view.LogoutView.as_view(), name='logout'),
