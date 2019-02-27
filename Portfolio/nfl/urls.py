71a172e8d2eb9e332d124b6d06d59ba66c3b7be8
Jan <Jan-Frings@gmx.de>
2019-02-27 12:47:26 +0100

    project_index layout optimized

--- Portfolio/nfl/urls.py
+++ Portfolio/nfl/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'nfl_stats'
+app_name = 'nfl'
 
 urlpatterns = [
     path('nfl_index', views.NflIndexView.as_view(), name='nfl_index')
