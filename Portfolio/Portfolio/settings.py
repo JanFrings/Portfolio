bf10847167d1ab7418dd83932bb2528ce0f61ade
Jan <Jan-Frings@gmx.de>
2019-05-26 12:11:19 +0200

    updata gitignore

--- Portfolio/Portfolio/settings.py
+++ Portfolio/Portfolio/settings.py
@@ -24,17 +24,12 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
-from decouple import config, Csv
-import dj_database_url
+from decouple import config
+
 
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
-}
+
+
 
 
 
